386cbd35b0cedb3f4e898031cca4ebfc2008911e
Tom B <44700588+Bailey-T@users.noreply.github.com>
2024-11-07 14:24:55 +0000

    Update install_master.sh

--- cluster-setup/latest/install_master.sh
+++ cluster-setup/latest/install_master.sh
@@ -184,8 +184,15 @@ mkdir -p ~/.kube
 sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config
 
 ### CNI
-kubectl apply -f https://raw.githubusercontent.com/killer-sh/cks-course-environment/master/cluster-setup/calico.yaml
+CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt)
+CLI_ARCH=amd64
+if [ "$(uname -m)" = "aarch64" ]; then CLI_ARCH=arm64; fi
+curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
+sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
+rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
 
+cilium install ${CILIUM_CLI_VERSION}
 
 # etcdctl
 ETCDCTL_VERSION=v3.5.1
