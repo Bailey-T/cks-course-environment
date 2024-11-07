a8adb4377adec646bc7bb9d46722b61ee541f50e
Tom B <44700588+Bailey-T@users.noreply.github.com>
2024-11-07 14:30:29 +0000

    Update install_worker.sh

--- cluster-setup/latest/install_worker.sh
+++ cluster-setup/latest/install_worker.sh
@@ -5,6 +5,7 @@
 set -e
 
 source /etc/lsb-release
+VERSION_ID=$DISTRIB_RELEASE
 if [ "$DISTRIB_RELEASE" != "20.04" ]; then
     echo "################################# "
     echo "############ WARNING ############ "
